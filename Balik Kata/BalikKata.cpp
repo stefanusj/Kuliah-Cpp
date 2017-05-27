5e7c7cb61905373e7fff60c00d4b43e643bc0e4e
Stefanus Julianto <stefanusj3@gmail.com>
2017-05-27 18:06:20 +0700

    Final State of App

---
+++ Balik Kata/BalikKata.cpp
@@ -0,0 +1,26 @@
+/**************************
+Nama : Stefanus Julianto
+Tanggal : 27 Mei 2017
+Program membalik 1 kata
+**************************/
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+char kata[99];
+
+string balik(char kata[]){
+    char temp[99]={""};
+    for(int i=0;i<strlen(kata);i++){
+        temp[i] = kata[strlen(kata)-i-1];
+    }
+    return temp;
+}
+
+main(){
+    string atak;
+    cout << "Masukkan kata : "; cin >> kata;
+    atak = balik(kata);
+    cout << "Kata setelah dibalik : " << atak;
+}
